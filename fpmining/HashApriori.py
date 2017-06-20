d5487ae76e6b9ad4fcb0258cd018bc16e9ffc96a
jcoral <1355539304@qq.com>
2017-06-20 21:18:03 +0800

    20-21

---
+++ fpmining/HashApriori.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+import hashlib
+
+
+def generateFreItemSet(freItemSet, originData, minSupport):
+    """
+
+    :param freItemSet: [i1, i2, ...]频繁项集
+    :return:
+    """
+
+    # [md5(item): [item: times, item: times, ...]]
+    box = dict()
+
+    # 组合所有的候选集
+    itemsLength = len(freItemSet)
+    allCandItemSet = list()
+    for h in range(itemsLength):
+        for f in range(h + 1, itemsLength):
+            # 合并两个项集
+            hitemSet = set(freItemSet[h].split(","))
+            fitemSet = set(freItemSet[f].split(","))
+            itemSet  = hitemSet.union(fitemSet)
+            allCandItemSet.append(itemSet)
+
+    # 遍历数据集
+    # 如果候选项集在事务当中，则将候选集添加到桶中
+    for itemSet in originData:
+        for candItem in allCandItemSet:
+            if candItem.issubset(itemSet):
+                candItemList = list(candItem)
+                candItemList.sort()
+                candItemStr = ",".join(candItemList)
+
+                md5 = hashlib.md5()
+                md5.update(candItemStr)
+                candItemMD = md5.hexdigest()
+
+                boxItems = box.get(candItemMD, dict())
+                boxItems[candItemStr] = boxItems.get(candItemStr, 0) + 1
+                box[candItemMD] = boxItems
+
+    # 过滤小于支持度的的项集
+    boxKeys = box.keys()
+    allFreItemSet = dict()
+    for key in boxKeys:
+        # 如果整个桶的长度小于支持度。则删除掉
+        if sum(box[key].values()) >= minSupport:
+            boxItems = box.get(key)
+            boxItemsKeys = boxItems.keys()
+            for boxItemsKey in boxItemsKeys:
+                candItemTimes = boxItems[boxItemsKey]
+                if candItemTimes >= minSupport:
+                    allFreItemSet[boxItemsKey] = candItemTimes
+                else:
+                    boxItems.__delitem__(boxItemsKey)
+        else:
+            box.__delitem__(key)
+
+    return allFreItemSet
+
+
+
+
+import Apriori as apriori
+
+originData = [
+    set(["I1", "I2", "I5"]),
+    set(["I2", "I4"]),
+    set(["I2", "I3"]),
+    set(["I1", "I2", "I4"]),
+    set(["I1", "I3"]),
+    set(["I2", "I3"]),
+    set(["I1", "I3"]),
+    set(["I1", "I2", "I3", "I5"]),
+    set(["I1", "I2", "I3"])
+]
+freFirstItemSet = apriori.generateFirstCandSet(originData)
+
+
+print generateFreItemSet(freFirstItemSet.keys(), originData, 2)
+
+
+
+
+
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import hashlib
+import itertools
 md5 = hashlib.md5()
 md5.update('123')
 print md5.hexdigest()
 
-
-
-
-
+md51 = hashlib.md5()
+md51.update('123')
+print md51.hexdigest()
 
 
 
